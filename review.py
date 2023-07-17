18b8375c49abba472e1617c4238c88a4d6b5a069
tomk142 <tomiishshalom@gmail.com>
2023-07-17 16:15:01 +0300

    s

--- review.py
+++ review.py
@@ -1,5 +1,5 @@
 def create_youtube_video(title,description)
-  	video = {"title"=title,"description"=description,likes=0,dislikes=0,comments={comment=""}}
+  	video = {"title"=title,"description"=description,likes=0,dislikes=0,comments[]}
 
 def like(video)
 	video[likes]+=1
@@ -8,4 +8,11 @@ def dislikes(video)
 	video[dislikes]+=1
 
 def add_comment(video,username,comment_text)
+	youtube_video=video
+	username=username
+	comment_text=comment_text
+	comments.append(username)
+	comments[0]=comment_text
 	
+
+
